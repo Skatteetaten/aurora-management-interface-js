77305dae8b239997425d708736afe47be6beb3f1
Espen Strømland <espen.stromland@gmail.com>
2017-10-12 12:28:01 +0200

    Repo review (#3)
    * changed /actuator to /
    
    * fixed tests
    
    * test watch all

--- test/middleware.test.ts
+++ test/middleware.test.ts
@@ -4,7 +4,7 @@ import { ManagementConfig } from "../src/config";
 
 describe("Management Interface middleware default config test", () => {
     const req = request();
-    const tests = ["/actuator", "/info", "/health"];
+    const tests = ["/", "/info", "/health"];
 
     tests.forEach(endpoint => {
         it(`Should return 200 OK at endpoint ${endpoint}`, () => {
