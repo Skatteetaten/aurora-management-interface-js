751dd36f009887b7f72eb4ce96ec683495e9a4ae
Espen Strømland <espen.stromland@skatteetaten.no>
2017-10-12 10:33:30 +0200

    fixed tests

--- test/actuatorEndpoint.test.ts
+++ test/actuatorEndpoint.test.ts
@@ -4,7 +4,7 @@ import { request } from "./helper";
 describe("Actuator endpoint Test", () => {
     it("Should contains links to info, health and self endpoint", () => {
         return request()
-            .get("/actuator")
+            .get("/")
             .expect(200)
             .then((res) => {
                 expect(res.body.links.length).toBe(3);
