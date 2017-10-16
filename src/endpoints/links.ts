e4a5c6e46ef7903ad8950a2f691a6394a30d9fcd
Espen Strømland <espen.stromland@skatteetaten.no>
2017-10-16 14:21:55 +0200

    LinkInfo type update

--- src/endpoints/links.ts
+++ src/endpoints/links.ts
@@ -1,9 +1,6 @@
 import { Request, Response, Router, RequestHandler } from "express";
 
-type LinkInfo = {
-    rel: string;
-    href: string;
-};
+type LinkInfo = { [index: string]: { href: string } };
 
 export const linksRequestHandler = (router: Router): RequestHandler => {
     return (req: Request, res: Response) => {
@@ -17,7 +14,7 @@ const getRoutes = (router: Router, req: Request): Array<LinkInfo> => {
     const ref = req.protocol + "://" + req.headers.host;
     const self = req.route.path;
 
-    return router.stack.reduce((result: any, it) => {
+    return router.stack.reduce((result: LinkInfo, it) => {
         const path = it.route.path;
         const rel = (path === self) ? "self" : path.replace("/", "");
         const href = ref + path;
