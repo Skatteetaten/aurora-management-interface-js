04187fab9c0c5b20ce9894a266a2c88003185236
Espen Strømland <espen.stromland@skatteetaten.no>
2018-10-05 08:13:29 +0200

    added async support

--- src/endpoints/health.ts
+++ src/endpoints/health.ts
@@ -1,39 +1,44 @@
-import { Request, Response, RequestHandler } from "express";
-import { Status, ManagementHealth, HealthStatus, HealthResult } from "../health";
-import { ManagementConfig } from "../config";
+import { Request, Response, RequestHandler } from 'express';
+import { Status, ManagementHealth, HealthStatus } from '../health';
+import { ManagementConfig } from '../config';
 
 export function healthRequestHandler(config: ManagementConfig): RequestHandler {
-    const managementHealth = fromConfig(config);
-    return (req: Request, res: Response) => {
-        const health = managementHealth.run();
-        const status = health["status"] as string;
-        res.status(getReturnCode(status)).json(health);
-    };
+  const managementHealth = fromConfig(config);
+  return (req: Request, res: Response) => {
+    const health = managementHealth.run();
+    health.then(async result => {
+      const status: string = result['status'];
+      res.status(getReturnCode(status)).json(result);
+    });
+  };
 }
 
 function fromConfig(config: ManagementConfig): ManagementHealth {
-    const managementHealth = new ManagementHealth();
-    managementHealth.cacheFor(config.cacheDuration);
+  const managementHealth = new ManagementHealth();
+  managementHealth.cacheFor(config.cacheDuration);
 
-    const checks = config.healthChecks;
-    Object.keys(checks).forEach(k => {
-        managementHealth.addCheck(() => {
-            const result = checks[k]();
-            const { status, ...fields } = result;
-            return new HealthStatus(k, Status.valueOf(status), fields);
-        });
+  const checks = config.healthChecks;
+  Object.keys(checks).forEach(k => {
+    managementHealth.addCheck(async () => {
+      const result = await checks[k]();
+      console.log(result);
+      const { status, ...fields } = result;
+      return new HealthStatus(k, Status.valueOf(status), fields);
     });
+  });
 
-    return managementHealth;
+  return managementHealth;
 }
 
 function getReturnCode(status: string): number {
-    switch (status) {
-        case "DOWN":
-        case "OUT_OF_SERVICE":
-        case "UNKNOWN": return 503;
+  switch (status) {
+    case 'DOWN':
+    case 'OUT_OF_SERVICE':
+    case 'UNKNOWN':
+      return 503;
 
-        case "COMMENT":
-        case "UP": return 200;
-    }
+    case 'COMMENT':
+    case 'UP':
+      return 200;
+  }
 }
--- src/health/managementHealth.ts
+++ src/health/managementHealth.ts
@@ -1,57 +1,65 @@
-import { HealthStatus, Status } from "./healthStatus";
+import { HealthStatus, Status } from './healthStatus';
 
 export type HealthResult = { [index: string]: any };
 
-export interface HealthCheckFunc {
-    (): HealthStatus;
-}
+export type HealthCheckFunc = () => HealthStatus | Promise<HealthStatus>;
 
 export class ManagementHealth {
-    private healthChecks: HealthCheckFunc[];
-    private validUntil: number;
-    private cachedHealthCheck: HealthResult;
-    private cacheDuration: number;
+  private healthChecks: HealthCheckFunc[];
+  private validUntil: number;
+  private cachedHealthCheck: HealthResult;
+  private cacheDuration: number;
 
-    constructor() {
-        this.healthChecks = [];
-        this.cacheDuration = 1000;
+  constructor() {
+    this.healthChecks = [];
+    this.cacheDuration = 1000;
+  }
+
+  addCheck(check: HealthCheckFunc): ManagementHealth {
+    this.healthChecks.push(check);
+    return this;
+  }
+
+  cacheFor(duration: number): ManagementHealth {
+    this.cacheDuration = duration;
+    return this;
+  }
+
+  async run(): Promise<HealthResult> {
+    const now = new Date().getTime();
+    const isCacheValid = now < this.validUntil;
+    if (
+      this.validUntil !== undefined &&
+      this.cachedHealthCheck !== undefined &&
+      isCacheValid
+    ) {
+      return this.cachedHealthCheck;
     }
 
-    addCheck(check: HealthCheckFunc): ManagementHealth {
-        this.healthChecks.push(check);
-        return this;
+    const result: HealthResult = {};
+    if (this.healthChecks.length === 0) {
+      result['status'] = Status[Status.UP];
+      return result;
     }
 
-    cacheFor(duration: number): ManagementHealth  {
-        this.cacheDuration = duration;
-        return this;
-    }
+    const healthResults: Promise<HealthStatus>[] = this.healthChecks.map(
+      async check => await check()
+    );
 
-    run(): HealthResult {
-        const now = new Date().getTime();
-        const isCacheValid = now < this.validUntil;
-        if (this.validUntil !== undefined && this.cachedHealthCheck !== undefined && isCacheValid) {
-            return this.cachedHealthCheck;
-        }
+    const healthStatuses = await Promise.all(healthResults);
+    const mainStatus = healthStatuses
+      .map(hr => hr.getStatus())
+      .sort()
+      .pop();
+    result['status'] = Status[mainStatus];
 
-        const result: HealthResult = {};
-        if (this.healthChecks.length === 0) {
-            result["status"] = Status[Status.UP];
-            return result;
-        }
+    healthStatuses.forEach(health => {
+      result[health.getName()] = health.getFields();
+    });
 
-        const healthResults: HealthStatus[] = this.healthChecks.map(c => c());
+    this.cachedHealthCheck = result;
+    this.validUntil = now + this.cacheDuration;
 
-        const mainStatus = healthResults.map(hr => hr.getStatus()).sort().pop();
-        result["status"] =  Status[mainStatus];
-
-        healthResults.forEach(health => {
-            result[health.getName()] =  health.getFields();
-        });
-
-        this.cachedHealthCheck = result;
-        this.validUntil = now + this.cacheDuration;
-
-        return result;
-    }
+    return result;
+  }
 }
