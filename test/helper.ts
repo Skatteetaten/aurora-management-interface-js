2e79aef30052d01546228281feb5007dfa77aae2
Martin Rødland <Martin.Rodland@skatteetaten.no>
2020-01-23 13:45:43 +0100

    added collectPrometheusMetrics

--- test/helper.ts
+++ test/helper.ts
@@ -2,10 +2,14 @@ import * as express from 'express';
 import * as supertest from 'supertest';
 
 import { IManagementConfig } from '../src/config';
-import { managementInterface } from '../src/middleware';
+import {
+  managementInterface,
+  collectPrometheusMetrics
+} from '../src/middleware';
 
 export function request(config?: IManagementConfig) {
   const app = express();
+  collectPrometheusMetrics;
   app.use(managementInterface(config));
   return supertest(app);
 }
